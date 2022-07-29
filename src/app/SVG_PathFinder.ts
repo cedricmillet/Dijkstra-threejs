0ff491d1b15d9334991efda0c289c7c420c3d065
Cédric <milletcedric83400@gmail.com>
2022-07-29 18:15:38 +0200

    dijkstra works

---
+++ src/app/SVG_PathFinder.ts
@@ -0,0 +1,220 @@
+import * as THREE from "three";
+import { SVGResultPaths } from "three/examples/jsm/loaders/SVGLoader";
+
+const DOT_PREFIX = "PATHFINDER_";
+
+export interface PathFinderPoint {
+    position:THREE.Vector3;
+    mesh:THREE.Mesh;
+    id: string;
+    neighbors: any[]
+}
+
+export class SVG_PathFinder {
+
+    private points : PathFinderPoint[] = [];
+
+    constructor() {
+
+    }
+
+    public getPoints = () => this.points;
+
+    private extractDataFromPath(path:SVGResultPaths) {
+        if(!path.userData || !(path.userData as any)?.node?.id)
+            throw new Error("Unable to retrieve id in path...");
+        
+        const id = (path.userData as any).node.id as string
+        if(!id.startsWith(DOT_PREFIX)) throw new Error("id must starts with DOT_PREFIX" + id);
+        
+        const chain = id.substring(DOT_PREFIX.length).split('__');
+        return {
+            id: chain[0],
+            neighbors: chain[1].split('-')
+        }
+    }
+
+    /**
+     * Add node in graph
+     * @param mesh 
+     */
+    public addPoint(mesh:THREE.Mesh, path:SVGResultPaths) {
+        const data = this.extractDataFromPath(path);
+        const position = SVG_PathFinder.calculateMeshPosition(mesh);
+        this.points.push({position, mesh, neighbors: data.neighbors, id: data.id});
+    }
+
+    /**
+     * 
+     * @param target 
+     */
+    public getNearestPointFrom(target:THREE.Vector3) : PathFinderPoint {
+        if(this.points.length===0) throw new Error("Empty point array");
+        let nearest = this.points[0];
+        let nearestDist = this.points[0].position.distanceTo(target);
+        for(let point of this.points) {
+            const distance = point.position.distanceTo(target);
+            if(distance < nearestDist) {
+                nearestDist = distance;
+                nearest = point;
+            }
+        }
+        return nearest;
+    }
+
+    /**
+     * 
+     */
+    public static isPartOfPathFinder(path:SVGResultPaths) : boolean {
+        const data = (path.userData as any).node;
+        return data?.id && data.id.startsWith(DOT_PREFIX);
+    }
+
+    private graph : any = {}
+
+    /**
+     * Recalculate graph (edge : distances between nodes)
+     */
+    public calculateGraph() {
+        this.graph = {} as any;
+        for(let point of this.points) {
+            let neighbors = {} as any;
+            for(let neighborId of point.neighbors) {
+                const neighbor = this.points.find(p=>p.id===neighborId)
+                if(!neighbor) throw new Error(`Unable to find neighbor#${neighborId}`);
+                neighbors[neighborId] = point.position.distanceTo(neighbor.position)
+            }
+            this.graph[point.id] = neighbors;
+        }
+    }
+
+    public getGraph = () => this.graph; 
+
+    /**
+     * Calculer le chemin de points nécéssaires 
+     * @param startingNodeId 
+     * @param endNodeId 
+     * @returns 
+     */
+    public getShortestPathBetween(startingNodeId:string, endNodeId:string) : THREE.Vector3[] {
+        //  Check Graph
+        if(!this.graph || Object.keys(this.graph).length === 0)
+            throw new Error(`Unable to find path in invalid graph, please call calculateGraph() before.`);
+        //  Check start/end nodes
+        if(!this.graph.hasOwnProperty(startingNodeId)) throw new Error(`Missing node#${startingNodeId} in provided graph`);
+        if(!this.graph.hasOwnProperty(endNodeId)) throw new Error(`Missing node#${endNodeId} in provided graph`);
+        //  Calculate
+        const path = Dijkstra.findShortestPath(this.graph, startingNodeId, endNodeId);
+        //  Return Path
+        return path.map((nodeId:string) => (this.points.find(p => p.id === nodeId) as PathFinderPoint).position);
+    }
+
+    /**
+     * Calculer la position (x,y,z) d'un Mesh à partir de sa géométrie
+     * @param mesh 
+     * @returns 
+     */
+    public static calculateMeshPosition(mesh:THREE.Mesh) {
+        const box = new THREE.Box3().setFromObject(mesh);
+        const result = new THREE.Vector3()
+        box.getCenter(result);
+        return result;
+    }
+
+    /**
+     * Calculer une courbe qui passe par un ensemble de points donné
+     * @param points Tableau de points de dimension 3 (x,y,z)
+     * @param color Couleur de la courbe
+     * @returns 
+     */
+    public static getCurveFromPoints(points: THREE.Vector3[], color:number=0xff0000) {
+        const curve = new THREE.CatmullRomCurve3( points );
+        const geometry = new THREE.BufferGeometry().setFromPoints( curve.getPoints( 50 ) );
+        const material = new THREE.LineBasicMaterial( { color } );
+        return new THREE.Line( geometry, material );
+    }
+
+
+}
+
+class Dijkstra {
+    
+    private static shortestDistanceNode = (distances:any, visited:any) => {
+        let shortest = null;
+        for (let node in distances) {
+            let currentIsShortest =
+                shortest === null || distances[node] < distances[shortest];
+            if (currentIsShortest && !visited.includes(node)) {
+                shortest = node;
+            }
+        }
+        return shortest;
+    };
+
+    public static findShortestPath = (graph:any, startNode:string, endNode:string) => {
+        // establish object for recording distances from the start node
+        let distances:any = {};
+        distances[endNode] = "Infinity";
+        distances = Object.assign(distances, graph[startNode]);
+
+        // track paths
+        let parents:any = { endNode: null };
+        for (let child in graph[startNode]) {
+            parents[child] = startNode;
+        }
+
+        // track nodes that have already been visited
+        let visited :any[] = [];
+
+        // find the nearest node
+        let node = this.shortestDistanceNode(distances, visited);
+
+        // for that node
+        while (node) {
+            // find its distance from the start node & its child nodes
+            let distance = distances[node];
+            let children = graph[node];
+            // for each of those child nodes
+            for (let child in children) {
+                // make sure each child node is not the start node
+                if (String(child) === String(startNode)) {
+                    continue;
+                } else {
+                    // save the distance from the start node to the child node
+                    let newdistance = distance + children[child];
+                    // if there's no recorded distance from the start node to the child node in the distances object
+                    // or if the recorded distance is shorter than the previously stored distance from the start node to the child node
+                    // save the distance to the object
+                    // record the path
+                    if (!distances[child] || distances[child] > newdistance) {
+                        distances[child] = newdistance;
+                        parents[child] = node;
+                    }
+                }
+            }
+            // move the node to the visited set
+            visited.push(node);
+            // move to the nearest neighbor node
+            node = this.shortestDistanceNode(distances, visited);
+        }
+
+        // using the stored paths from start node to end node
+        // record the shortest path
+        let shortestPath = [endNode];
+        let parent = parents[endNode];
+        while (parent) {
+            shortestPath.push(parent);
+            parent = parents[parent];
+        }
+        shortestPath.reverse();
+
+        // return the shortest path from start node to end node & its distance
+        /*
+        let results = {
+            distance: distances[endNode],
+            path: shortestPath,
+        };*/
+
+        return shortestPath;
+    };
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,114 @@
-import { Component } from '@angular/core';
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import {SVGLoader, SVGResult} from 'three/examples/jsm/loaders/SVGLoader';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { SVG_PathFinder } from './SVG_PathFinder';
+import { Box3 } from 'three';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dijkstra-threejs';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('rendererContainer') rendererContainer!: ElementRef;
+
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  scene:THREE.Scene;
+  camera:THREE.Camera;
+  controls:OrbitControls;
+  pathFinder:SVG_PathFinder = new SVG_PathFinder();
+
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = null as any;
+    this.controls = null as any;
+  }
+
+  ngAfterViewInit(): void {
+    const [w,h] = this.getSize();
+    //  Renderer
+    this.renderer.setSize(w, h);
+    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+    this.scene.background = new THREE.Color( 0xffffff );
+    //  Light
+    this.scene.add(new THREE.DirectionalLight(0xffffff, 0.5));
+    this.scene.add(new THREE.AmbientLight(0x404040));
+    //  Helpers
+    this.scene.add(new THREE.GridHelper(20, 10));
+    this.scene.add(new THREE.AxesHelper(10));
+    //  Camera
+    this.camera = new THREE.PerspectiveCamera(75, w/h, 1, 1000);
+    this.camera.position.set(5,10,5);
+    this.camera.lookAt(0,0,0);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.loadSVG();
+    //  Run !
+    this.animate();
+  }
+
+  ngOnInit(): void {}
+
+  private animate() {
+    window.requestAnimationFrame(() => this.animate());
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera as THREE.Camera);
+  }
+
+
+  private getSize() {
+    const e = this.rendererContainer.nativeElement as HTMLElement;
+    return [e.offsetWidth, e.offsetHeight]
+  }
+
+  private loadSVG() {
+    const that = this;
+    const loader = new SVGLoader();
+    loader.load( `assets/img.svg`, function (data:SVGResult) {
+      const paths = data.paths;
+      const group = new THREE.Group();
+      for(let path of paths) {
+        const shapes = SVGLoader.createShapes(path);
+        const geometry = new THREE.ExtrudeGeometry(shapes, {steps:1, depth:-5});
+        const material = new THREE.MeshPhongMaterial({color: new THREE.Color(path.color), side: THREE.DoubleSide});
+        const mesh = new THREE.Mesh(geometry, material);
+        group.add(mesh);
+
+        if(SVG_PathFinder.isPartOfPathFinder(path)) {
+          that.pathFinder.addPoint(mesh, path);
+        }
+      }
+
+      that.applyCommonTransformation(group);
+      
+      //group.translateOnAxis(new THREE.Vector3(1,1,0), 0)
+      that.scene.add(group);
+
+      
+      that.pathFinder.calculateGraph()
+      
+      const itinerary = that.pathFinder.getShortestPathBetween("A", "F")
+      const curve = SVG_PathFinder.getCurveFromPoints(itinerary);
+      that.applyCommonTransformation(curve);
+      that.scene.add(curve);
+
+    }, function(xhr:any) {
+      console.log("loaded : ", xhr.loaded/xhr.total*100, "%");
+    }, function(err) {
+      console.error(err);
+    })
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event : any) {
+    this.renderer.setScissor(event.target.innerWidth, event.target.innerHeight);
+  }
+
+  //  Transformer l'échelle et la position pour aligner les objets sur la grille
+  private applyCommonTransformation(obj:THREE.Group|THREE.Object3D) {
+    obj.rotateX(Math.PI/2);
+    const s = 0.05;
+    obj.scale.set(s,s,s)
+  }
 }
+
